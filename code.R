dc7be5342b8bde34637e985cb87f98dff985c6a5
Arqam Usman Ali <aa358@njit.edu>
2024-04-22 03:15:55 -0400

    Updated code.R with fixed code

--- code.R
+++ code.R
@@ -1,35 +1,38 @@
 # *** code ***
 
-# Install and load required packages
-install.packages("textclean")
-install.packages("dplyr")
-install.packages("purrr")
-install.packages("quanteda")
-
-library(textclean)
-library(dplyr)
-library(purrr)
-library(quanteda)
-
-# Load AFINN lexicon
 afinn <- read.delim("https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-en-165.txt", header = FALSE, sep = "\t", col.names = c("word", "score"))
 
-# Function to calculate sentiment score of a text
-calculate_sentiment <- function(text, lexicon) {
-  tokens <- tokens(text, remove_punct = TRUE)
-  tokens <- tokens_tolower(tokens)
-  scores <- sum(tokens_lookup(tokens, lexicon$word, lexicon$score, nomatch = 0))
-  return(scores)
+analyze_sentiment <- function(sentence, afinn_lexicon) {
+  # Tokenize the sentence into words
+  words <- strsplit(tolower(sentence), "\\s+")[[1]]
+  
+  # Initialize sentiment score
+  sentiment_score <- 0
+  
+  # Calculate sentiment score for each word
+  for (word in words) {
+    if (word %in% afinn_lexicon$word) {
+      sentiment_score <- sentiment_score + afinn_lexicon$score[afinn_lexicon$word == word]
+    }
+  }
+  
+  # Return the sentiment score
+  return(sentiment_score)
 }
 
-# Example text
-text1 <- "This is a very bad movie. I loved it!"
-text2 <- "I'm feeling really happy today!"
-text3 <- "The weather is terrible. I hate rainy days."
-text4 <- "I'm thrilled to hear the news! It's fantastic!"
-
-# Calculate sentiment score
-sentiment_score <- calculate_sentiment(text4, afinn)
-
-# Print sentiment score
+# Example usage
+text1 <- "I love this movie! It's fantastic."
+text2 <- "This is a very bad movie. I loved it!"
+text3 <- "I'm feeling really happy today!"
+text4 <- "The weather is terrible. I hate rainy days."
+text5 <- "I'm thrilled to hear the news! It's fantastic!"
+sentiment_score <- analyze_sentiment(text1, afinn)
+print(sentiment_score)
+sentiment_score <- analyze_sentiment(text2, afinn)
+print(sentiment_score)
+sentiment_score <- analyze_sentiment(text3, afinn)
+print(sentiment_score)
+sentiment_score <- analyze_sentiment(text4, afinn)
+print(sentiment_score)
+sentiment_score <- analyze_sentiment(text5, afinn)
 print(sentiment_score)
